785953502a64631b71a34a226fd84af7e3d7981d
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-10-07 18:21:37 +0300

    Updated login route

--- server/app.py
+++ server/app.py
@@ -26,6 +26,23 @@ def sign_up():
 
             return jsonify({"message": "New User created successfully"}), 201
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"error": "Username and password are required"}), 400
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.validatepassword(password):
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"error": "Invalid username or password"}), 401
+
 @app.route('/')
 def home():
     return 'Welcome'
